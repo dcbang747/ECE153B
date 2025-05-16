10c0074ee244c2409a37d224628ae46dd5b8c357
d-m441 <dmaharjan@ucsb.edu>
2025-05-15 21:24:24 -0700

    etyyuyrt

--- Template_L5/L5B/src/DAC.c
+++ Template_L5/L5B/src/DAC.c
@@ -9,7 +9,7 @@ static void DAC_Pin_Init(void)
     /* PA4 → analog (DAC1_OUT1) */
     RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
 
-    GPIOA->MODER |=  GPIO_MODER_MODE4;      /* 11b = analog */
+    GPIOA->MODER &= ~GPIO_MODER_MODE4;      
     GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD4;
     GPIOA->ASCR  |=  GPIO_ASCR_ASC4;
 }
@@ -19,7 +19,7 @@ void DAC_Write_Value(uint32_t value)
 {
     /* right-aligned 12-bit data register (DHR12R1) */
     value &= 0x0FFFUL;                      /* clamp to 12 bits */
-		DAC1->DHR12R1 &= value;
+		DAC1->DHR12R1 &= ~DAC_DHR12R1_DACC1DHR;
     DAC1->DHR12R1 = value;
 }
 
@@ -43,5 +43,5 @@ void DAC_Init(void)
     /* enable channel 1 */
     DAC1->CR |= DAC_CR_EN1;
 
-    DAC_Write_Value(200);
+    DAC_Write_Value(500);
 }
--- Template_L5/L5B/src/EXTI.c
+++ Template_L5/L5B/src/EXTI.c
@@ -6,34 +6,45 @@
 #define DAC_MAX    4095U
 #define DAC_INCREMENT 256U
 
-volatile uint32_t dac_value = 0;
+volatile int dac_value = 000;
 static enum { DOWN, UP } direction = UP;
 
 /*------------------------------------------------------------------*/
-void EXTI_Init(void)
-{
-    /* 1. Enable clocks ---------------------------------------------------- */
-    RCC->AHB2ENR  |= RCC_AHB2ENR_GPIOCEN;     /* GPIOC clock       */
-    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;     /* *** SYSCFG clock ***/
+void EXTI_Init(void) {
+	// Initialize User Button
+	// [TODO]
+		GPIOC -> MODER &= ~GPIO_MODER_MODE13;
+		GPIOC -> PUPDR &= ~GPIO_PUPDR_PUPD13;
+		GPIOA -> ODR |= GPIO_ODR_OD5;
+	
+	// Configure SYSCFG EXTI
+	// [TODO]
+		RCC->AHB2ENR  |= RCC_AHB2ENR_GPIOCEN;     // PC13 clock
+		RCC->APB2ENR  |= RCC_APB2ENR_SYSCFGEN;    // SYSCFG clock
+		SYSCFG->EXTICR[3] &= ~(0xF << 4);         // clear EXTI13[7:4]
+		SYSCFG->EXTICR[3] |=  (2   << 4);         // 0b0010 = port C
 
-    /* 2. Map EXTI line 13 to PC13 ---------------------------------------- */
-    SYSCFG->EXTICR[3] &= ~SYSCFG_EXTICR4_EXTI13;
-    SYSCFG->EXTICR[3] |=  SYSCFG_EXTICR4_EXTI13_PC;
-
-    /* 3. Select edge(s) --------------------------------------------------- */
-    EXTI->FTSR1 |= EXTI_FTSR1_FT13;    /* comment this line if you want both */
-
-    /* 4. Unmask & NVIC ---------------------------------------------------- */
-		EXTI->PR1|= EXTI_PR1_PIF13;
-    EXTI->IMR1  |=  EXTI_IMR1_IM13;     /* line enable    */
-    NVIC_EnableIRQ(EXTI15_10_IRQn);
-    NVIC_SetPriority(EXTI15_10_IRQn, 0);
+	
+	// Configure EXTI Trigger
+	// [TODO]
+	    EXTI->RTSR1 &= ~EXTI_RTSR1_RT13;          // disable rising edge
+    	EXTI->FTSR1 |=  EXTI_FTSR1_FT13;          // enable falling edge
+	
+	// Enable EXTI
+	// [TODO]
+    	EXTI->IMR1  |=  EXTI_IMR1_IM13;           // unmask EXTI13
+	
+	// Configure and Enable in NVIC
+	// [TODO]
+		NVIC_SetPriority(EXTI15_10_IRQn, 0);
+		NVIC_EnableIRQ  (EXTI15_10_IRQn);
 }
 
-/*------------------------------------------------------------------*/
+// [TODO] Write Interrupt Handlers (look in startup_stm32l476xx.s to find the 
+// interrupt handler names that you should use)
 void EXTI15_10_IRQHandler(void)
-{        /* clear flag */
-        dac_value += DAC_INCREMENT;
+{
+        dac_value += 256;
         DAC_Write_Value(dac_value);
 				
 				EXTI->PR1|= EXTI_PR1_PIF13;
