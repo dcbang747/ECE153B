fdcdd1c7e1ba459e16337bedcbbb569926900392
dcbang747 <31807533+dcbang747@users.noreply.github.com>
2025-05-16 12:38:38 -0700

    e

--- Template_L5/L5B/src/EXTI.c
+++ Template_L5/L5B/src/EXTI.c
@@ -10,34 +10,32 @@ volatile int dac_value = 000;
 static enum { DOWN, UP } direction = UP;
 
 /*------------------------------------------------------------------*/
-void EXTI_Init(void) {
-	// Initialize User Button
-	// [TODO]
-		GPIOC -> MODER &= ~GPIO_MODER_MODE13;
-		GPIOC -> PUPDR &= ~GPIO_PUPDR_PUPD13;
-		GPIOA -> ODR |= GPIO_ODR_OD5;
-	
-	// Configure SYSCFG EXTI
-	// [TODO]
-		RCC->AHB2ENR  |= RCC_AHB2ENR_GPIOCEN;     // PC13 clock
-		RCC->APB2ENR  |= RCC_APB2ENR_SYSCFGEN;    // SYSCFG clock
-		SYSCFG->EXTICR[3] &= ~(0xF << 4);         // clear EXTI13[7:4]
-		SYSCFG->EXTICR[3] |=  (2   << 4);         // 0b0010 = port C
+void EXTI_Init(void)
+{
+    /* 1. Clocks ----------------------------------------------------*/
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;      /* GPIOC */
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;     /* SYSCFG */
 
-	
-	// Configure EXTI Trigger
-	// [TODO]
-	    EXTI->RTSR1 &= ~EXTI_RTSR1_RT13;          // disable rising edge
-    	EXTI->FTSR1 |=  EXTI_FTSR1_FT13;          // enable falling edge
-	
-	// Enable EXTI
-	// [TODO]
-    	EXTI->IMR1  |=  EXTI_IMR1_IM13;           // unmask EXTI13
-	
-	// Configure and Enable in NVIC
-	// [TODO]
-		NVIC_SetPriority(EXTI15_10_IRQn, 0);
-		NVIC_EnableIRQ  (EXTI15_10_IRQn);
+    /* 2. PC13 pin --------------------------------------------------*/
+    GPIOC->MODER &= ~GPIO_MODER_MODE13;       /* input  */
+    GPIOC->PUPDR &= ~GPIO_PUPDR_PUPD13;
+    GPIOC->PUPDR |=  GPIO_PUPDR_PUPD13_1;     /* pull-up */
+
+    /* 3. Route EXTI13 to port C -----------------------------------*/
+    SYSCFG->EXTICR[3] &= ~SYSCFG_EXTICR4_EXTI13;
+    SYSCFG->EXTICR[3] |=  SYSCFG_EXTICR4_EXTI13_PC;          /* 0b0010 = C */
+
+    /* 4. Edge + mask ----------------------------------------------*/
+    EXTI->FTSR1 |=  EXTI_FTSR1_FT13;          /* falling on  */
+    EXTI->RTSR1 &= ~EXTI_RTSR1_RT13;          /* rising off  */
+    EXTI->IMR1  |=  EXTI_IMR1_IM13;           /* unmask      */
+
+    /* 5. Clear any stale pending bit ------------------------------*/
+    EXTI->PR1 = EXTI_PR1_PIF13;
+
+    /* 6. NVIC ------------------------------------------------------*/
+    NVIC_SetPriority(EXTI15_10_IRQn, 0);
+    NVIC_EnableIRQ  (EXTI15_10_IRQn);
 }
 
 // [TODO] Write Interrupt Handlers (look in startup_stm32l476xx.s to find the 
