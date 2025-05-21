2416d42ed3064c937cb6c165b47b28c41aa830a8
d-m441 <dmaharjan@ucsb.edu>
2025-05-20 19:46:56 -0700

    saved

--- Template_L6/L6B/src/CRC.c
+++ Template_L6/L6B/src/CRC.c
@@ -17,14 +17,17 @@
 uint32_t CRC_CalcBlockCRC(const uint32_t * pBuffer, uint32_t BufferLength) {
   uint32_t index = 0;
   // write all data into data register
-  return 0;  // read CRC from data register
+	for(int i=0;i<BufferLength;i++){
+		CRC->DR = pBuffer[i];
+	}
+  return CRC->DR;  // read CRC from data register
 }	
 
 void CRC_Init(void) {
     RCC->AHB1ENR  |= RCC_AHB1ENR_CRCEN;            /* enable peripheral clock */
     CRC->CR        = CRC_CR_RESET;                 /* soft reset              */
-    CRC->POL       = CRC_POLY;                     /* generator polynomial    */
-    CRC->INIT      = CRC_INIT_VAL;                 /* starting value          */
+    CRC->POL       = 0x104C11DB7;                     /* generator polynomial    */
+    CRC->INIT      = CRC_INIT_INIT;                 /* starting value          */
     CRC->CR       &= ~CRC_CR_POLYSIZE;             /* 00 → 32-bit polynomial  */
 }
 
--- Template_L6/L6B/src/LED.c
+++ Template_L6/L6B/src/LED.c
@@ -13,16 +13,29 @@
 
 void LED_Init(void){
 	//TODO
+	//Enables Clock for PORT A and C
+		RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN; 
+		RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
+	
+	// Initialize Green LED
+	// [TODO]
+	GPIOA->MODER &= ~GPIO_MODER_MODE5; //Clear LED GPIO pin
+	GPIOA->MODER |= GPIO_MODER_MODE5_0; // Set LED to output 3.3v
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT5; // Push-pull mode
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD5; // No Pull up/pull down
 }
 
 void LED_On(void){
 	//TODO
+	GPIOA->ODR |= GPIO_ODR_OD5;
 }
 
 void LED_Off(void){
 	//TODO
+	GPIOA->ODR &= ~GPIO_ODR_OD5;
 }
 
 void LED_Toggle(void){
 	//TODO
+	GPIOA->ODR ^= GPIO_ODR_OD5;
 }
