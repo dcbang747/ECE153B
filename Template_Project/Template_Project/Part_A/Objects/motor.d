f1e230ecdcb66e48fd71aa8164c3c4b64a8467de
d-m441 <dmaharjan@ucsb.edu>
2025-06-04 21:24:11 -0700

    goofy

--- Template_Project/Template_Project/Part_A/Objects/motor.d
+++ Template_Project/Template_Project/Part_A/Objects/motor.d
@@ -1,3 +1,10 @@
 ./objects/motor.o: src\motor.c src\stm32l476xx.h \
-  C:\Users\denik\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Keil_v5\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Keil_v5\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Keil_v5\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  C:\Keil_v5\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\m-profile\cmsis_armclang_m.h \
+  C:\Keil_v5\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
   src\motor.h
